47b3590e1c753dcdddad77577727e698f8edc9c4
weixy <weixiny2008@gmail.com>
2014-10-17 10:45:39 +1300

    v0.0.4 - Add class for hooks generation.

---
+++ src/main/java/net/weixy/autotest/selenium/grammar/generator/HooksGenerator.java
@@ -0,0 +1,14 @@
+package net.weixy.autotest.selenium.grammar.generator;
+
+import net.weixy.autotest.selenium.psi.ObjectMapFile;
+
+/**
+ * Created by Jim Wei on 24/09/14.
+ */
+public class HooksGenerator {
+
+    public HooksGenerator(ObjectMapFile file, String sourcePath, String outputPath) {
+
+        //final List<ObjectMap>
+    }
+}
